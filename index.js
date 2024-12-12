65d5440f0bdbd2d52e5a3f6a35d345d29912314e
GITCHEA002 <chhunsokchea317@gmail.com>
2024-12-12 18:36:49 +0700

    second-commit

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const jsonserver = require("json-server");
 const server = jsonserver.create();
 const rounter = jsonserver.router("DB.json");
 const middleWare = jsonserver.defaults();
+const cors = require('cors')
 
 server.use(cors());
 server.use(rounter);
 server.use(middleWare);
 
-const port = 3000;
+const port = 3001;
 server.listen(port,function(){
-    console.log(`http//localhost:${port}`);
+    console.log(`http://localhost:${port}`);
 })
